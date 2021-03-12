ccb3e414f047d3b23c79ab4527def61def4a6eb0
Gerald Versluis <gerald@verslu.is>
2021-03-12 14:44:48 +0100

    Implement SwipeView

--- XFSwipeViewSample/MainPage.xaml.cs
+++ XFSwipeViewSample/MainPage.xaml.cs
@@ -10,9 +10,23 @@ namespace XFSwipeViewSample
 {
     public partial class MainPage : ContentPage
     {
+        public List<string> MyItems { get; set; } = new List<string>()
+        {
+            "Hi there",
+            "Please subscribe",
+            "to my channel"
+        };
+
         public MainPage()
         {
             InitializeComponent();
+
+            BindingContext = this;
+        }
+
+        void SwipeItem_Invoked(System.Object sender, System.EventArgs e)
+        {
+            DisplayAlert("Swipe!", "Invoked", "OK");
         }
     }
 }
